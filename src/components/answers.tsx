0af2b316751284f4c930b76cc16124012eec056f
koch21 <hagnakoch@gmail.com>
2021-05-24 16:01:03 -0300

    replace set up

--- src/components/answers.tsx
+++ src/components/answers.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Field, Form, Formik } from "formik";
 // Styles
 import { Container, Answerswrapper, PickRow } from '../styles/pages/answers'
@@ -19,36 +19,67 @@ export function Answers(props) {
       <Answerswrapper>
         <Formik
           initialValues={{
-            picked: '',
+            picked: '' || true || false,
           }}
-          onSubmit={async (values) => {
-            await new Promise((r) => setTimeout(r, 500));
-            alert(JSON.stringify(values, null, 2));
+          onSubmit={(values) => {
           }}
         >
           {({ values }) => (
             <Form>
-              <div id="my-radio-group" />
               <div role="group" aria-labelledby="my-radio-group">
                 <h5>{category}</h5>
-                <p>{question.replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"')}</p>
+                <p>{question.replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"').replace(/&Delta;/g, '"')}</p>
                 <PickRow>
-                  <label>
-                    <Field type="radio" name="picked" value={correct_answer} />
-                    {`${correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"')}`}
-                  </label>
-                  <label>
-                    <Field type="radio" name="picked" value={incorrect_answers[0]} />
-                    {`${incorrect_answers[0].replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"')}`}
-                  </label>
-                  <label>
-                    <Field type="radio" name="picked" value={incorrect_answers[1]} />
-                    {`${incorrect_answers[1].replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"')}`}
-                  </label>
-                  <label>
-                    <Field type="radio" name="picked" value={incorrect_answers[2]} />
-                    {`${incorrect_answers[2].replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"')}`}
-                  </label>
+                  {(() => {
+                    if (correct_answer === true || false) {
+                      return (
+                        <div>
+                          <label>
+                            <Field type="radio" name="picked" value={correct_answer} />
+                            {`${correct_answer}`}
+                          </label>
+                          <label>
+                            <Field type="radio" name="picked" value={incorrect_answers[0]} />
+                            {`${incorrect_answers[0]}`}
+                          </label>
+                        </div>
+                      )
+                    } else {
+                      return (
+                        <div>
+                          <label>
+                            <Field type="radio" name="picked" value={correct_answer} />
+                            {`${correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"').replace(/&Delta;/g, '"')}`}
+                          </label>
+                          <label>
+                            <Field type="radio" name="picked" value={incorrect_answers[0]} />
+                            {`${incorrect_answers[0].replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"').replace(/&Delta;/g, '"')}`}
+                          </label>
+                        </div>
+                      )
+                    }
+                  })()}
+
+                  {(() => {
+                    if (incorrect_answers[1] === undefined) {
+                      return (
+                        <div></div>
+                      )
+                    } else {
+                      return (
+                        <div>
+                          <label>
+                            <Field type="radio" name="picked" value={incorrect_answers[1]} />
+                            {`${incorrect_answers[1].replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"').replace(/&Delta;/g, '"')}`}
+                          </label>
+                          <label>
+                            <Field type="radio" name="picked" value={incorrect_answers[2]} />
+                            {`${incorrect_answers[2].replace(/&quot;/g, '"').replace(/&#039;/g, '"').replace(/&eacute;/g, '"').replace(/&Delta;/g, '"')}`}
+                          </label>
+                        </div>
+                      )
+                    }
+                  })()}
                 </PickRow>
               </div>
             </Form>
